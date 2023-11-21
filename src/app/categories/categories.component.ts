edb8880eb74fda6efb3cfd3dda1dfb7079ca8f22
Lexsak <bartoszpiprek123@gmail.com>
2023-11-21 23:12:50 +0100

    loadData

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../services/categories.service';
 import { Category } from '../models/category';
 
@@ -7,46 +7,33 @@ import { Category } from '../models/category';
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css'],
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categoryArray: any[] = [];
+  formCategory: string = '';
+  formStatus: string = 'Add';
+
   constructor(private categoryService: CategoriesService) {}
 
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe((val) => {
+      console.log(val);
+
+      this.categoryArray = val;
+    });
+  }
+
   onSubmit(formData: any) {
-    
     let categoryData: Category = {
       category: formData.value.category,
     };
 
-    this.categoryService.saveData(categoryData)
+    this.categoryService.saveData(categoryData);
 
+    formData.reset();
+  }
 
-    // let subCategoryData = {
-    //   subCategory: 'subCategory1',
-    // };
-    // this.afs
-    //   .collection('categories')
-    //   .add(categoryData)
-    //   .then((docRef) => {
-    //     console.log(docRef);
-    //     this.afs
-    //       .collection('categories')
-    //       .doc(docRef.id)
-    //       .collection('subcategories')
-    //       .add(subCategoryData)
-    //       .then((docRef1) => {
-    //         console.log(docRef1);
-
-    //         this.afs
-    //           .collection('categories')
-    //           .doc(docRef.id)
-    //           .collection('subcategories')
-    //           .doc(docRef1.id).collection('subsubcategories').add(subCategoryData).then(docRef2 => {
-    //             console.log('Second level subcategory saved successfully');
-                
-    //           });
-    //       });
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+  onEdit(category: any) {
+    this.formCategory = category
+    this.formStatus = "Edit"
   }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -19,6 +20,21 @@ export class CategoriesService {
       .catch((error) => {
         console.log(error);
       });
+  }
 
+  loadData() {
+    return this.afs
+      .collection('categories')
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data() ;
+            const id = a.payload.doc.id;
+
+            return { id, data };
+          });
+        })
+      );
   }
 }
