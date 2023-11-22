78136e8201dc70212507c3385120834dcd5aaa11
Lexsak <bartoszpiprek123@gmail.com>
2023-11-22 23:14:26 +0100

    login logic

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userEmail: string = '';
+  isLoggedIn$: Observable<boolean> = of(false);
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const userString = localStorage.getItem('user');
+
+    if (userString !== null) {
+      const user = JSON.parse(userString);
+      this.userEmail = user.email;
+    } else {
+      console.log('User is not available in localStorage');
+    }
+
+    this.isLoggedIn$ = this.authService.isLoggedIn();
+  }
+
+  onLogOut() {
+    this.authService.logOut();
+  }
 }
